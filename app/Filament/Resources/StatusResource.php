27f11a33b6740d30684fcb08e613b142c05378ee
fahmi widya <fahmiwidyaa@gmail.com>
2025-03-08 02:28:21 +0700

    feat: add OrderStatus CRUD

---
+++ app/Filament/Resources/StatusResource.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\StatusResource\Pages;
+use App\Filament\Resources\StatusResource\RelationManagers;
+use App\Models\Status;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class StatusResource extends Resource
+{
+    protected static ?string $model = Status::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('nama_status')
+                    ->maxLength(255),
+                Forms\Components\ColorPicker::make('warna_teks_status')
+                    ->placeholder('gunakan warna gelap'),
+                Forms\Components\ColorPicker::make('warna_background_status')
+                    ->placeholder('gunakan warna yang lebih terang'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\ColorColumn::make('nama_status')
+                    ->copyable()
+                    ->copyableState(fn(string $state): string => "Color: {$state}")
+                    ->searchable(),
+                Tables\Columns\ColorColumn::make('warna_teks_status')
+                    ->copyable()
+                    ->copyableState(fn(string $state): string => "Color: {$state}")
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('warna_background_status')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStatuses::route('/'),
+            'create' => Pages\CreateStatus::route('/create'),
+            'edit' => Pages\EditStatus::route('/{record}/edit'),
+        ];
+    }
+}

b55bc2e530addb5f5e381678cd8621fb6cb66d59
fahmi widya <fahmiwidyaa@gmail.com>
2025-03-08 08:51:07 +0700

    style: kategori sorting simplified & add search bar

--- resources/js/Pages/POS.vue
+++ resources/js/Pages/POS.vue
@@ -60,43 +60,44 @@ onMounted(() => {
                 </div>
             </div>
             <div class="flex flex-row h-auto w-full px-4 py-4 gap-4">
-                <div @click="toggleActive('all')" :class="['flex flex-col py-3 px-3 w-40 h-36 rounded-2xl justify-between cursor-pointer',activeMenu === 'all' ? 'bg-[#f0f7ff] outline outline-2 outline-[#2D71F8]' : 'bg-white']">
+                <div @click="toggleActive('all')" :class="['flex flex-row py-3 px-3 w-48 h-20 rounded-2xl cursor-pointer',activeMenu === 'all' ? 'bg-[#f0f7ff] outline outline-2 outline-[#2D71F8]' : 'bg-white']">
                     <div :class="['category_icon flex justify-center items-center w-14 h-14 rounded-full',activeMenu === 'all' ? 'bg-[#2D71F8] text-white' : 'bg-gray-100 text-gray-500']">
                         <i class="ri-restaurant-2-line text-xl text-current"></i>
                     </div>
-                    <div class="wrap flex flex-col gap-1">
-                        <div class="nama_kategori font-semibold text-gray-700">All Menu</div>
+                    <div class="wrap flex flex-col ml-3 justify-center">
+                        <div class="nama_kategori font-semibold text-lg text-gray-700">All Menu</div>
                         <div class="total_barang_in_kategori text-sm text-gray-500">8 items</div>
                     </div>
                 </div>
 
                 <!-- Drinks -->
-                <div @click="toggleActive('drinks')" :class="['flex flex-col py-3 px-3 w-40 h-36 rounded-2xl justify-between cursor-pointer',activeMenu === 'drinks' ? 'bg-[#f0f7ff] outline outline-2 outline-[#2D71F8]' : 'bg-white']">
+                <div @click="toggleActive('drinks')" :class="['flex flex-row py-3 px-3 w-48 h-20 rounded-2xl cursor-pointer',activeMenu === 'drinks' ? 'bg-[#f0f7ff] outline outline-2 outline-[#2D71F8]' : 'bg-white']">
                     <div :class="['category_icon flex justify-center items-center w-14 h-14 rounded-full',activeMenu === 'drinks' ? 'bg-[#2D71F8] text-white' : 'bg-gray-100 text-gray-500']">
                         <i class="ri-cup-line text-xl text-current"></i>
                     </div>
-                    <div class="wrap flex flex-col gap-1">
-                        <div class="nama_kategori font-semibold text-gray-700">Drinks</div>
+                    <div class="wrap flex flex-col ml-3 justify-center">
+                        <div class="nama_kategori font-semibold text-lg text-gray-700">Drinks</div>
                         <div class="total_barang_in_kategori text-sm text-gray-500">3 items</div>
                     </div>
                 </div>
                 <!-- Dessert -->
-                <div @click="toggleActive('dessert')" :class="['flex flex-col py-3 px-3 w-40 h-36 rounded-2xl justify-between cursor-pointer',activeMenu === 'dessert' ? 'bg-[#f0f7ff] outline outline-2 outline-[#2D71F8]' : 'bg-white']">
+                <div @click="toggleActive('dessert')" :class="['flex flex-row py-3 px-3 w-48 h-20 rounded-2xl cursor-pointer',activeMenu === 'dessert' ? 'bg-[#f0f7ff] outline outline-2 outline-[#2D71F8]' : 'bg-white']">
                     <div :class="['category_icon flex justify-center items-center w-14 h-14 rounded-full',activeMenu === 'dessert' ? 'bg-[#2D71F8] text-white' : 'bg-gray-100 text-gray-500']">
                         <i class="ri-cake-3-line text-xl text-current"></i>
                     </div>
-                    <div class="wrap flex flex-col gap-1">
-                        <div class="nama_kategori font-semibold text-gray-700">Dessert</div>
+                    <div class="wrap flex flex-col ml-3 justify-center">
+                        <div class="nama_kategori font-semibold text-lg text-gray-700">Dessert</div>
                         <div class="total_barang_in_kategori text-sm text-gray-500">3 items</div>
                     </div>
                 </div>
             </div>
+            <!-- search -->
             <div class="flex items-center justify-center w-[97%] h-auto px-4 bg-white rounded-full h-16 py-2 mx-auto">
-            <input type="text" class="flex-1 bg-transparent px-4 placeholder:text-gray-400 text-gray-700 font-semibold border-none focus:outline-none focus:ring-0" placeholder="Search something sweet on your mind..."/>
-            <div class="icon flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full shrink-0 text-gray-700">
-                <i class="bi bi-search text-lg text-current"></i>
+                <input type="text" class="flex-1 bg-transparent px-4 placeholder:text-gray-400 text-gray-700 font-semibold border-none focus:outline-none focus:ring-0" placeholder="Search something sweet on your mind..."/>
+                    <div class="icon flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full shrink-0 text-gray-700">
+                    <i class="bi bi-search text-lg text-current"></i>
+                </div>
             </div>
-        </div>
 
         </div>
         <div class="flex w-[30%] h-screen bg-white shadow-2xl shadow-slate-100">

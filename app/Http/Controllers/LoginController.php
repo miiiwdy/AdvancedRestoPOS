ee60c7292349984f8eff0d7f2c498e00eca31523
fahmi widya <fahmiwidyaa@gmail.com>
2025-04-15 23:08:17 +0700

    fix: reworking on login with shift functionality

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,28 +9,26 @@ use Carbon\Carbon;
 
 class LoginController extends Controller
 {
-
     public function index()
     {
         if (Auth::user()->hasRole(4)) {
             $now = Carbon::now();
-    
             $Shift = DataShift::where([
                 ['restos_id', '=', Auth::user()->restos_id],
                 ['outlets_id', '=', Auth::user()->outlets_id],
                 ['no_shift', '=', Auth::user()->shift]
-                ])->first();
-                
-            $currentHour = $now->format('H:i');
-            $ShiftStart = $Shift->shift_start;
-            $ShiftEnd = $Shift->shift_end;
-    
-            $ShiftValidate = $currentHour >= $ShiftStart && $currentHour < $ShiftEnd;
+            ])->first();
+            $start = Carbon::createFromFormat('H:i:s', $Shift->shift_start)->setDate($now->year, $now->month, $now->day);
+            $end = Carbon::createFromFormat('H:i:s', $Shift->shift_end)->setDate($now->year, $now->month, $now->day);
+            if ($end->lessThan($start)) {
+                $end->addDay();
+            }
+            $ShiftValidate = $now->between($start, $end);
         }
 
         if (auth()->user()->hasRole(1) || auth()->user()->hasRole(2) || auth()->user()->hasRole(3)) {
             return Inertia::location('/admin');
-        } else if(Auth::user()->hasRole(4) && $ShiftValidate) {
+        } else if (Auth::user()->hasRole(4) && isset($ShiftValidate) && $ShiftValidate) {
             return Inertia::location('/pos');
         } else {
             Auth::logout();
